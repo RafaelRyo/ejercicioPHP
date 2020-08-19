3322f34e4803c93fc18b594a6f86e17b981dafca
Rafael Manjarres Villanueva <rafaelmanjarres1995@gmail.com>
2020-08-19 03:56:06 -0500

    se modifica el readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ejercicioPHP
+
+Este proyecto utiliza el framework laravel, etiquetas Html, estilos CSS y php
+
+###Para ejecutarlo
+php artisan serve
--- public/paginas/hamburguesa.php
+++ public/paginas/hamburguesa.php
@@ -1,5 +1,3 @@
 <div class="row">
-    <h5>Menu general de hamburguesas</h5>
 
-    <img src="./images/hamburguesa-cerdo.jpg" border="0" width="50%">
 </div>
